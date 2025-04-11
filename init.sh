a618fbd111a3c4a3d25be60edf6b0515f3548145
khalil hadji <h-adji_tech@proton.me>
2025-04-11 22:55:30 +0200

    logs

--- init.sh
+++ init.sh
@@ -31,7 +31,10 @@ else
     )
     sudo apt install -y "${toInstall[@]}"
 fi
+cecho green "Cloning setup repos"
+cecho blue "DOTFILES"
 [ ! -d "dotfiles" ] && git clone https://github.com/H-ADJI/dotfiles
+cecho blue "CYBORG"
 [ ! -d "cyborg" ] && git clone https://github.com/H-ADJI/cyborg
 cd cyborg || exit 1
 chmod +x install.sh

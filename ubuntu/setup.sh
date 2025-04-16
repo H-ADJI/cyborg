71df8d5a5c03fb2fc3cd981bc81c4f4797f3a155
khalil hadji <h-adji_tech@proton.me>
2025-04-16 17:54:04 +0200

    fix packages

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -4,6 +4,28 @@ installpackages() {
         sudo nala install -y "$package"
         cecho green "[DONE] Installing $package"
     done <~/cyborg/ubuntu/packages.txt
+    cecho blue "[START] INSTALL brave"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL mako"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL rustup"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL starship"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL swappy"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL transcrypt"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL fonts"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL uv"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL brave"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL brave"
+    chsh -s "$(which zsh)"
+    cecho blue "[START] INSTALL brave"
+    chsh -s "$(which zsh)"
 }
 # main entrypoint
 launch_setup() {
